dadf41a223ac53b5d480e6b5363bba1b1cf19b51
Aaron <goolyaaron@gmail.com>
2019-02-18 23:22:35 +0000

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+module.exports = (client) => {
+  console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+  console.log("Bot Made by aaronokiay");
+  console.log("http://aaronokiay.glitch.me");
+
+}
